3f57e64c27b6cccb6476543c11d59db69c0f527f
thapaRoyal <thaparoyal17@gmail.com>
2023-03-04 07:37:09 -0500

    creating a placeholder component

---
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.css']
+})
+export class PlaceholderComponent {
+
+}
