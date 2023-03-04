e312d7220b200491ba3a0e94fce4a2bee02d0140
thapaRoyal <thaparoyal17@gmail.com>
2023-03-04 05:58:22 -0500

    Adding basic routing

--- src/app/elements/elements-routing.module.ts
+++ src/app/elements/elements-routing.module.ts
@@ -1,10 +1,11 @@
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'elements', component: ElementsHomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ElementsRoutingModule { }
+export class ElementsRoutingModule {}
