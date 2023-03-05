6c49394a0c07199e5e3992e9302c48799656f41f
thapaRoyal <thaparoyal17@gmail.com>
2023-03-05 22:59:21 +0545

    implementing a titled divider

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -5,10 +5,11 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
-  imports: [CommonModule, ElementsRoutingModule],
+  imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: [],
 })
 export class ElementsModule {}
