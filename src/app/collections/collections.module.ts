252c9e3ba99382935de3c71648cc800e009d847f
thapaRoyal <thaparoyal17@gmail.com>
2023-03-06 23:54:38 +0545

    creating a displaying a table

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionHomeComponent } from './collection-home/collection-home.component';
+import { TableComponent } from './table/table.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [CollectionHomeComponent],
-  imports: [CommonModule, CollectionsRoutingModule],
+  declarations: [CollectionHomeComponent, TableComponent],
+  imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   exports: [],
 })
 export class CollectionsModule {}
