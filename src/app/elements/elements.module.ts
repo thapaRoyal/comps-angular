dbc675d6dddfb5e0f92ae4c0705e3956b6f78575
thapaRoyal <thaparoyal17@gmail.com>
2023-03-06 11:49:58 +0545

    creating a segment component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -6,9 +6,10 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
 import { SharedModule } from '../shared/shared.module';
+import { SegmentComponent } from './segment/segment.component';
 
 @NgModule({
-  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
+  declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective, SegmentComponent],
   imports: [CommonModule, ElementsRoutingModule, SharedModule],
   exports: [],
 })
